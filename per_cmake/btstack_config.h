4bab4ea6be62aba482affcca8bb2a400241c8cfe
mazent <massimo.zennaro@texa.com>
2017-03-22 14:05:09 +0100

    Adeguato

--- per_cmake/btstack_config.h
+++ per_cmake/btstack_config.h
@@ -26,6 +26,8 @@
 #define ENABLE_LOG_INFO
 #define ENABLE_LOG_ERROR
 
+#define MAX_NR_LE_DEVICE_DB_ENTRIES		1
+
 // BTstack configuration. buffers, sizes, ...
 #define HCI_ACL_PAYLOAD_SIZE 52
 #define MAX_SPP_CONNECTIONS 1
